7b51c1091555fb10615bef3feafe1e0abd86e976
kittu4522 <gopiisboss@gmail.com>
2020-04-07 11:11:55 +0530

    first commit

---
+++ BusSeatsCollectionView/Seats.swift
@@ -0,0 +1,18 @@
+//
+//  Seats.swift
+//  BusSeatsCollectionView
+//
+//  Created by RAM on 06/04/20.
+//  Copyright © 2020 RAM. All rights reserved.
+//
+
+import UIKit
+
+class Seats: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
